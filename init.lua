af0ce2c98d12b445e635ce57262619b8a8163fa2
jm <jm@duka_arch>
2025-09-01 13:34:11 -0300

    Paso configs a option.lua

--- init.lua
+++ init.lua
@@ -1,19 +1,3 @@
-vim.g.mapleader = " " -- Set leader to spacebar
-vim.opt.number = true -- Show line numbers
-vim.opt.cursorline = true -- cursor line
-
-vim.opt.undofile = true -- historial de undos
-
-vim.opt.ignorecase = true -- case-insensitive searching
-vim.opt.smartcase = true
-
--- comentarios en php con //
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "php" },
-  callback = function()
-    vim.bo.commentstring = "// %s"
-  end,
-})
-
+require("config.options")
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,19 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+vim.g.mapleader = " " -- Set leader to spacebar
+vim.opt.number = true -- Show line numbers
+vim.opt.cursorline = true -- cursor line
+
+vim.opt.undofile = true -- historial de undos
+
+vim.opt.ignorecase = true -- case-insensitive searching
+vim.opt.smartcase = true
+
+-- integration works automatically. Requires Neovim >= 0.10.0
+vim.opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" -- Sync with system clipboard
+
+-- comentarios en php con //
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "php" },
+  callback = function()
+    vim.bo.commentstring = "// %s"
+  end,
+})
